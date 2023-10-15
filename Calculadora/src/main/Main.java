925cd2157f8064f0d5ddb0d8b38632a10f74651f
Caye110905 <145046977+Caye110905@users.noreply.github.com>
2023-10-15 20:46:05 +0200

    Calculadora v4

--- Calculadora/src/main/Main.java
+++ Calculadora/src/main/Main.java
@@ -12,23 +12,23 @@ public class Main {
 	 */
 	public static void main (String[] args) {
 		// Paso 1: Declarar variables para almacenar los números ingresados y el resultado. 
-		double num1, num2, resultado;
+		double nume1, nume2, resultado;
 		// Paso2: Importar la clase Scanner para obtener entrada del usuario.
 		Scanner scanner = new Scanner(System.in);
 		
 		// Paso 3: Pedir al usuario que ingrese el primer número.
 		System.out.print("Ingresa el primer número: ");
-		num1 = scanner.nextDouble();
+		nume1 = scanner.nextDouble();
 		
 		// Paso 4: Pedir al usuario que ingrese el segundo número.
 		System.out.print("Ingresa el segundo número: ");
-		num2 = scanner.nextDouble();
+		nume2 = scanner.nextDouble();
 		
 		// Paso 5: Calcular la suma de los dos números.
-		resultado = num1 + num2;
+		resultado = nume1 + nume2;
 		
 		// Paso 6: Mostrar el resultado.
-		System.out.println("La suma de " + num1 + " y " + num2 + " es igual a " + resultado);
+		System.out.println("La suma de " + nume1 + " y " + nume2 + " es igual a " + resultado);
 		
 		// Paso 7: Cerrar el scanner.
 		scanner.close ();
